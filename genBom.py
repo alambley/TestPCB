f54855bb093c1aa1442a13e4a3f35159e2dd68cf
Alexander Lambley <lambleyalexander@gmail.com>
2021-05-13 13:50:34 -0400

    - generated BOM csv now has components sorted by amount in schematic for ease of ordering - added Y character for grabbing crystals from schematic

--- genBom.py
+++ genBom.py
@@ -10,7 +10,7 @@ def first_string_in_quotes(string):
         return temp[1]
     return None
 
-FORMAT = "%(asctime)s %(levelname)s %(message)s"
+FORMAT = "%(asctime)s %(levelname)7s %(message)s"
 log.basicConfig(format=FORMAT, level=log.INFO)
 
 log.debug("Command Arguments: {}".format(sys.argv))
@@ -40,9 +40,9 @@ for line in f:
     elif start_reading:
         current_schematic.append(line.replace("\n",""))
 
-log.info("Found {} schematics".format(len(schematics)))
+log.info("Found {} elements".format(len(schematics)))
 
-grab_schem_ref = ["C","R","L","U", "J", "D", "SW"]
+grab_schem_ref = ["C","R","L","U", "J", "D", "SW", "Y"]
 log.debug("Schematics that did not match grab_schem_ref...")
 
 i = 0
@@ -62,7 +62,7 @@ while i < len(schematics):
 i = 0
 while i < len(schematics):
     if "Mfr" not in schematics[i][7] or len(schematics[i]) < 8:
-        log.warning("Mfr tag missing from {}".format(schematics[i][0]))
+        log.warning("Mfr tag missing from {}".format(first_string_in_quotes(schematics[i][3])))
         del schematics[i]
     else:
         i += 1
@@ -75,25 +75,33 @@ for sch in schematics:
         if first_string_in_quotes(sch[7]) == to_csv[x][2] and first_string_in_quotes(sch[8]) == to_csv[x][3]:
             already_exists = True
             to_csv[x][4] += 1
+            to_csv[x][5].append(first_string_in_quotes(sch[3]))
             break
     if already_exists:
         continue
     temp = []
+    names = []
     temp.append(list(filter(lambda x: x.isalpha(), sch[0].split(" ")[2]))[0])
     temp.append(first_string_in_quotes(sch[4]))
     temp.append(first_string_in_quotes(sch[7]))
     temp.append(first_string_in_quotes(sch[8]))
     temp.append(1)
+    temp.append([first_string_in_quotes(sch[3])])
     to_csv.append(temp.copy())
 
 to_csv.sort(key = lambda x: x[4], reverse=True)
 
-bom_file = os.path.join(os.path.dirname(sch_path), "bom.csv")
+for x in range(len(to_csv)):
+    to_csv[x][5].sort()
+    for y in range(len(to_csv[x][5]) - 1):
+        to_csv[x][5][y] = to_csv[x][5][y] + ","
+    to_csv[x][5] = "".join(to_csv[x][5])
 
-print(to_csv)
-print(bom_file)
+bom_file = os.path.join(os.path.dirname(sch_path), "bom.csv")
 
 file = open(bom_file, 'w', newline ='')
 for element in to_csv:
     write = csv.writer(file) 
-    write.writerow(element) 
+    write.writerow(element)
+
+log.info("BOM .csv successfully written to {}".format(bom_file))
